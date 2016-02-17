c8e9d2459ed7697ee6f0a1a49f4d016208a198ed
ivan <ivan>
2016-02-17 12:19:27 +0100

    Trying the implementation of facebook login

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -10,8 +10,8 @@ import hello.views
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
+    url(r'^fblogin', include('hello.urls')),
     url(r'^$', hello.views.index, name='index'),
-    url(r'^fblogin', hello.views.fblogin),
     url(r'^db', hello.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
 ]
