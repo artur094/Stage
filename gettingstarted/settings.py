c57082d3e623fb29c42ca41890cb7ccb430f9e6d
ivan <ivan>
2016-02-18 12:31:39 +0100

    Implementing Database

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello'
+    'hello',
 )
 
 SESSION_ENGINE = ('django.contrib.sessions.backends.file')
