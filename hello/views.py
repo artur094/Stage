8d3d5e7b24d6d2da3460fb2172af5c236b382964
ivan <ivan>
2016-02-18 17:14:30 +0100

    Implementing Database

--- hello/views.py
+++ hello/views.py
@@ -43,7 +43,8 @@ def fb_profile(request):
     user.name = me['name']
     user.email = me['email']
     user.birthday = me['birthday']
-    user.save()
+    if Utente.objects.filter(id=user.id).exists():
+        user.save()
     request.session['user'] = user
     request.session['token'] = token
     return render(request, 'profile.html', {'person' : user })
