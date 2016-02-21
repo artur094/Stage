918ea6c53471fc60b75727b8abbb41d3ddb2cc30
ivan <ivan>
2016-02-21 18:28:02 +0100

    Adding friends

--- hello/views.py
+++ hello/views.py
@@ -100,7 +100,7 @@ def fbfriends(request):
             user_friend.name = friend['name']
             user_friend.save()
         fbfriend.friend = user_friend
-        if Friend.objects.filter(Q(user_id=fbfriend.user.id) & Q(friend_id=fbfriend.id)).count() <= 0:
+        if Friend.objects.filter(Q(user_id=fbfriend.user.id), Q(friend_id=fbfriend.id)).exists():
             return HttpResponse("User: "+fbfriend.user.id+"<br />Friend: "+fbfriend.friend.id)
             #fbfriend.save()
 
