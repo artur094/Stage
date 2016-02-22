acbf1e97916899a002858275ac62d8dbb1d10e29
ivan <ivan>
2016-02-22 09:10:55 +0100

    Adding Posts

--- hello/views.py
+++ hello/views.py
@@ -151,7 +151,6 @@ def fbposts(request):
 
         if not Post.objects.filter(id_post = p.id_post).exists():
             p.save()
-
         return render(request, 'test.html', {'posts': posts['data']})
 
 
