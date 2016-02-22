30d6afe2603733fad78582fdc1f457922a53ca2e
ivan <ivan>
2016-02-22 10:26:47 +0100

    Implementing Instagram

--- hello/instagram_views.py
+++ hello/instagram_views.py
@@ -4,10 +4,9 @@ from models import *
 client_id = '5afea7f15ea94a7cbf602fcdd54b0526'
 
 def login(request):
-    url_inst = 'https://api.instagram.com/oauth/authorize/?client_id='+client_id+'&redirect_uri='
-    url_profile = 'https://facebookalgorithm.herokuapp.com/instagram/profile'
-    url = url_inst+url_profile+'&response_type=token'
-    return render(request, 'instagram_login.html', {'url': url})
+    redirect_uri = 'https://facebookalgorithm.herokuapp.com/instagram/profile'
+    url = 'https://api.instagram.com/oauth/authorize/?client_id='+client_id+'&redirect_uri='+redirect_uri+'&response_type=code'
+    return render(request, 'url')
 
 def profile(request):
     return render(request, 'instagram_profile.html')
