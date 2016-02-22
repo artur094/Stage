ec2dea8aa559828d36e945c7bf801978c646e814
ivan <ivan>
2016-02-22 15:05:36 +0100

    Implementing Instagram

--- hello/instagram_views.py
+++ hello/instagram_views.py
@@ -30,7 +30,7 @@ def login(request):
              'code':request.GET['code']
         }
         r = requests.post(url, params=data)
-        return render(request, 'instagram_profile.html', {'token':r.json()})
+        return render(request, 'instagram_profile.html', {'token':data})
         #return HttpResponseRedirect(url)
     else:
         data = json.dump(request.json)
