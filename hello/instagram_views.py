447ffe54f69927d3434038bc419edd5f955b4562
ivan <ivan>
2016-02-22 14:24:12 +0100

    Implementing Instagram

--- hello/instagram_views.py
+++ hello/instagram_views.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from django.shortcuts import *
 from django.http import HttpResponseRedirect
 from instagram.client import InstagramAPI
@@ -9,24 +10,30 @@ client_secret = '1a861ce3f62547db9af64ac889af45d3'
 
 def login(request):
     redirect_uri = 'https://facebookalgorithm.herokuapp.com/instagram/'
-    if 'error' not in request.GET and 'code' not in request.GET:
+    if 'error' not in request.GET and 'code' not in request.GET and 'access_token':
         url = 'https://api.instagram.com/oauth/authorize/?client_id='+client_id+'&redirect_uri='+redirect_uri+'&response_type=code'
         return HttpResponseRedirect(url)
     elif 'error' in request.GET:
         return HttpResponse("ERROR")
     elif 'code' in request.GET :
-        url = 'https://api.instagram.com/oauth/access_token'
-        data = {
-            'client_id': client_id,
-            'client_secret':client_secret,
-            'grant_type':'authorization_code',
-            'redirect_uri':redirect_uri,
-            'code':request.GET['code']
-        }
-        r = requests.get(url, params=data)
-        return render(request, 'instagram_profile.html', {'token':r.json()})
+        url = 'https://api.instagram.com/oauth/access_token?'
+        url += 'client_id='+client_id+'&'
+        url += 'client_secret='+client_id+'&'
+        url += 'grant_type='+client_id+'&'
+        url += 'client_id='+client_id+'&'
+        url += 'client_id='+client_id
+        # data = {
+        #     'client_id': client_id,
+        #     'client_secret':client_secret,
+        #     'grant_type':'authorization_code',
+        #     'redirect_uri':redirect_uri,
+        #     'code':request.GET['code']
+        # }
+        # r = requests.get(url, params=data)
+        return HttpResponseRedirect(url)
     else:
-        return HttpResponse("ERROR")
+        data = json.dump(request.json)
+        return render(request, 'instagram_profile.html', {'token':data['access_token']})
 
 
 
