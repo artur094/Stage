af33ba150e3a2989039a70498c9e59131d645e86
ivan <ivan>
2016-02-22 10:43:11 +0100

    Implementing Instagram

--- hello/instagram_views.py
+++ hello/instagram_views.py
@@ -1,3 +1,4 @@
+import requests
 from django.shortcuts import *
 from django.http import HttpResponseRedirect
 from models import *
@@ -7,7 +8,8 @@ client_id = '5afea7f15ea94a7cbf602fcdd54b0526'
 def login(request):
     redirect_uri = 'https://facebookalgorithm.herokuapp.com/instagram/profile'
     url = 'https://api.instagram.com/oauth/authorize/?client_id='+client_id+'&redirect_uri='+redirect_uri+'&response_type=code'
-    return request.get('url')
+    r = requests.get('url')
+    return render(request, 'instagram_login.html', {'info':r})
 
 def profile(request):
     return render(request, 'instagram_profile.html')
