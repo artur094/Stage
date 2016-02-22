2c7dc8b4f9783de46c844c3d91231e980361e969
ivan <ivan>
2016-02-22 16:05:51 +0100

    Implementing Instagram

--- hello/instagram_views.py
+++ hello/instagram_views.py
@@ -7,42 +7,26 @@ from models import *
 
 client_id = '5afea7f15ea94a7cbf602fcdd54b0526'
 client_secret = '1a861ce3f62547db9af64ac889af45d3'
-
+redirect_uri = 'https://facebookalgorithm.herokuapp.com/instagram/profile'
 def login(request):
-    dati = {}
-    try:
-        dati = request.json()
-    except Exception as e:
-        pass
-    redirect_uri = 'https://facebookalgorithm.herokuapp.com/instagram/'
-    if 'error' not in request.GET and 'code' not in request.GET and 'access_token' not in dati and 'error_type' not in dati:
-        url = 'https://api.instagram.com/oauth/authorize/?client_id='+client_id+'&redirect_uri='+redirect_uri+'&response_type=code'
-        r = requests.get(url)
-        return render(request, 'instagram_profile.html', {'token': r.text, 'info': url})
-        #return HttpResponseRedirect(url)
-    elif 'error' in request.GET or 'error_type' in dati:
-        return HttpResponse("ERROR")
-    elif 'code' in request.GET:
-        url = 'https://api.instagram.com/oauth/access_token'
-        data = {
-             'client_id': client_id,
-             'client_secret':client_secret,
-             'grant_type': 'authorization_code',
-             'redirect_uri':redirect_uri,
-             'code':request.GET['code'],
-        }
-        r = requests.post(url,data=data)
-
-        return render(request, 'instagram_profile.html', {'token': r.json(), 'info': data})
-        #return HttpResponseRedirect(url)
-    elif 'access_token':
-        return HttpResponse("ERROR")
-
-
-
+    url = 'https://api.instagram.com/oauth/authorize/?client_id='+client_id+'&redirect_uri='+redirect_uri+'&response_type=code'
+    return HttpResponseRedirect(url)
 
 
 def profile(request):
-    token = request.GET['code']
-    api = InstagramAPI(access_token=token, client_secret=client_secret)
-    return render(request, 'instagram_profile.html', {'token':token})
+    if 'error' not in request.GET and 'code' not in request.GET:
+        return login(request)
+    if 'error' in request.GET:
+        return HttpResponse("Errore")
+
+    url = 'https://api.instagram.com/oauth/access_token'
+    data = {
+        'client_id': client_id,
+        'client_secret':client_secret,
+        'grant_type': 'authorization_code',
+        'redirect_uri':redirect_uri,
+        'code':request.GET['code'],
+    }
+    r = requests.post(url,data=data)
+
+    return render(request, 'instagram_profile.html', {'token':r.json()})
