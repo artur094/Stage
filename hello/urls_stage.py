779cf91a177d02b98c9bf8d48e7692206b7c6361
ivan <ivan>
2016-03-30 09:35:23 +0200

    Re-implementing instagram for mariage

--- hello/urls_stage.py
+++ hello/urls_stage.py
@@ -5,7 +5,5 @@ from . import views_stage
 urlpatterns = [
     url(r'^$', views_stage.login),
     url(r'^profile', views_stage.profile),
-    url(r'^follows', views_stage.follows),
-    url(r'^followed', views_stage.followedby),
 ]
 
