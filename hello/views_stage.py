d8e3358a5097432bee6f53d8d917e5677080fe92
ivan <ivan>
2016-04-07 10:08:42 +0200

    Added css and changed html page for instagram profile

--- hello/views_stage.py
+++ hello/views_stage.py
@@ -43,14 +43,135 @@ def test(request):
     #test = Coppia.objects.all()
 
     profile = {
-        'id':'id-utente',
-        'username': 'username',
-        'full_name': 'name surname',
-        'bio':'This is my biographie, Hello World!',
+        'id':'048284812394',
+        'username': 'Gallet',
+        'full_name': 'Gallo Fallet',
+        'bio':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         'profile_picture':'http://www.filastrocche.it/contenuti/wp-content/uploads/2002/01/gallo-400.jpg'
     }
 
-    posts = []
+    posts = [
+        {
+            'caption':
+                {
+                    'created_time':'1459535128',
+                    'from':{
+                        'full_name': 'Gallo Fallet'
+                    },
+                    'text':'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?'
+                },
+            'user_has_liked':False,
+            'type':'Image',
+            'location':{
+                'name':'Arco',
+                'longitude':'10.887777778',
+                'latitude':'45.921666667',
+            },
+            'likes':{
+                'count':'14'
+            },
+            'users_in_photo':[
+                {
+                    'user':{
+                        'username': 'Gallet',
+                        'full_name':'Gallo Fallet'
+                    }
+                },
+                {
+                    'user': {
+                        'username': 'Gronka',
+                        'full_name': 'Pollo Fellet'
+                    }
+                },
+                {
+                    'user': {
+                        'username': 'Canga',
+                        'full_name': 'Cagur Canger'
+                    }
+                },
+            ],
+            'tags':[
+                'tag1','tag2','tag3','tag4','tag1','tag2','tag3','tag4'
+            ]
+        },
+        {
+            'caption':
+                {
+                    'created_time': '1459535128',
+                    'from': {
+                        'full_name': 'Gallo Fallet'
+                    },
+                    'text': 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?'
+                },
+            'user_has_liked': False,
+            'type': 'Image',
+            'location': {
+                'name': 'Arco',
+                'longitude': '10.887777778',
+                'latitude': '45.921666667',
+            },
+            'likes': {
+                'count': '14'
+            },
+            'users_in_photo': [
+                {
+                    'user': {
+                        'username': 'Gallet',
+                        'full_name': 'Gallo Fallet'
+                    }
+                },
+                {
+                    'user': {
+                        'username': 'Gronka',
+                        'full_name': 'Pollo Fellet'
+                    }
+                },
+                {
+                    'user': {
+                        'username': 'Canga',
+                        'full_name': 'Cagur Canger'
+                    }
+                },
+            ],
+            'tags': [
+                'tag1', 'tag2', 'tag3', 'tag4', 'tag1', 'tag2', 'tag3', 'tag4'
+            ]
+        }
+    ]
+
+    # {% for post in posts %}
+    #                 <div>
+    #                     <div>Date: {{ post.caption.created_time }}</div>
+    #                     <div>Created By: {{ post.caption.from.full_name }}</div>
+    #                     <div>Type: {{ post.type }}</div>
+    #                     <div>Do I like this? {{ post.user_has_liked }}</div>
+    #                     {% if 'name' in post.location %}
+    #                         <div>Posto: {{ post.location.name }}</div>
+    #                     {% endif %}
+    #                     {% if 'latitude' in post.location and 'longitude' in post.location %}
+    #                         <div>Location: Longitudine: {{ post.location.longitude }} - Latitudine: {{ post.location.latitude }}</div>
+    #                     {% endif %}
+    #                     <div>Testo: {{ post.caption.text }}</div>
+    #                     <div>Likes: {{ post.likes.count }}</div>
+    #                     <ul>
+    #                         {% for user in post.users_in_photo %}
+    #                             <li>{{ user.user.username }} - {{ user.user.full_name }}</li>
+    #                         {% endfor %}
+    #                     </ul>
+    #                     <div>
+    #                         Tag:
+    #                         <div>
+    #                             <ul>
+    #                                 {% for tag in post.tags %}
+    #                                     {{ tag }},
+    #                                 {% endfor %}
+    #                             </ul>
+    #                         </div>
+    #                     </div>
+    #                 </div>
+    #                 <hr />
+    #             {% endfor %}
+
 
     return render(request, 'social/instagram_profile.html', {'token': 'token', 'profile':profile, 'posts':posts})
 
