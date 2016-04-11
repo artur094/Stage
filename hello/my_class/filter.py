1f962955118cdcdc1c2aa1b347713965b08bfa95
ivan <ivan>
2016-04-11 15:44:27 +0200

    Fixed date.. I hope

---
+++ hello/my_class/filter.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+from django import template
+
+register = template.Library()
+
+@register.filter("timestamp")
+def timestamp(value):
+    try:
+        return datetime.fromtimestamp(value/1000).strftime('%Y-%m-%d %H:%M:%S')
+    except AttributeError, e:
+        return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
