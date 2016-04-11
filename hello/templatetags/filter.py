eb8a1590298deace78aab2cf8c819bd5ea856fe8
ivan <ivan>
2016-04-11 16:14:28 +0200

    Maybe I've fixed the data

--- hello/templatetags/filter.py
+++ hello/templatetags/filter.py
@@ -7,6 +7,6 @@ register = template.Library()
 @register.filter("timestamp")
 def timestamp(value):
     try:
-        return datetime.fromtimestamp(value).strftime('%Y-%m-%d %H:%M:%S')
+        return datetime.fromtimestamp(float(value)).strftime('%Y-%m-%d %H:%M:%S')
     except AttributeError, e:
         return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
