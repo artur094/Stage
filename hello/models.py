11d8ba077f319c8f638d98f68e932f34c1deff90
ivan <ivan>
2016-04-27 10:23:51 +0200

    Fixing db with heroku

--- hello/models.py
+++ hello/models.py
@@ -4,7 +4,7 @@ import django
 from gettingstarted.settings import DATE_INPUT_FORMATS
 
 
-# Create your models here.
+# Create your models here. :)
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
 
