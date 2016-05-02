d4a9ab9478569f315618f44bf965c75f883b7ea8
ivan <ivan>
2016-05-02 10:57:16 +0200

    Trying new filter on selection page

--- hello/templatetags/filter.py
+++ hello/templatetags/filter.py
@@ -12,3 +12,10 @@ def timestamp(value):
         return datetime.now().strftime('%d-%m-%Y %H:%M:%S')
     except ValueError, e:
         return '01-01-1970 00:00:00'
+
+def array_to_string(array):
+    str = ''
+    for value in array:
+        str += value + ' '
+    str = str[:len(str)-1]
+    return str
