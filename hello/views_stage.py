0d8235fbc09b7487e515515cd7f96c581d929ac2
ivan <ivan>
2016-05-06 15:39:10 +0200

    Now relatives should works fine, i hope

--- hello/views_stage.py
+++ hello/views_stage.py
@@ -651,9 +651,9 @@ def settings(request):
         return HttpResponse('Settings saved')
 
     my_categories = Category.objects.all().filter(rsa=me).exclude(name="relatives' post")
-    relatives = Relative.objects.all().filter(rsa=me)
+    relatives =  Category.objects.all().filter(rsa=me).get(name="relatives' post")
 
-    return render(request, 'settings.html', { 'user':me, 'categories': my_categories,'relatives_name':"relatives' post",'relatives':relatives})
+    return render(request, 'settings.html', { 'user':me, 'categories': my_categories,'relatives':relatives})
 
 def previous(request):
     return render(request, 'previous.html')
